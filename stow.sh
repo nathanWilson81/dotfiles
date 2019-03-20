2c2d831e253a6692eb120f826233a1a0d8eee4ea
Nathan Wilson <nwilson@MB-PRO-1814.local>
2019-03-19 19:28:08 -0600

    Add kitty

--- stow.sh
+++ stow.sh
@@ -1,2 +1,3 @@
-stow -t ~/.config/nvim nvim # Links neovim configuration
+mkdir ~/.config/nvim && stow -t ~/.config/nvim nvim # Links neovim configuration
 stow -t ~/ zsh # Links zshrc 
+mkdir ~/.config/kitty && stow -t ~/.config/kitty kitty # Links kitty terminal config
